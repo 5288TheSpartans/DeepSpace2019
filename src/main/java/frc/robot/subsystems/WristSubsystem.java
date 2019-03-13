698b83edfacd75c362a83d66846741e215bf5c83
Keyon Jerome <keyonjerome@gmail.com>
2019-03-13 11:36:07 -0400

    hi its yamaan

--- src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -48,7 +48,7 @@ public class WristSubsystem extends Subsystem {
   }
 
   public double getRotationAngle() { // get the rotation angle return
-    return (((wristMotor.getSelectedSensorPosition()/encoderUnit)* (1/gearRatio)))*360;
+    return (((wristMotor.getSelectedSensorPosition()/encoderUnit)/(gearRatio)))*360;
   }
 
   public boolean isWristAtTop() {
