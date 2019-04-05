e2a11a7004a14618441767e3b3e275a4b7609d32
yamaan <yamaan93@gmail.com>
2019-04-05 19:23:02 -0400

    removing pneumatics

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -236,7 +236,7 @@ public class Robot extends TimedRobot {
 //    SmartDashboard.putNumber("Secondary RightStickX", m_oi.getSecondaryControllerRightStickX());
     SmartDashboard.putBoolean("Arm Limit Switch", arm.getLimitSwitch());
     SmartDashboard.putBoolean("Wrist Limit Switch", wrist.getLimitSwitch());
-    SmartDashboard.putBoolean("Intake Solenoid", pneumatics.getIsExtended());
+   // SmartDashboard.putBoolean("Intake Solenoid", pneumatics.getIsExtended());
     SmartDashboard.putNumber("Wrist Distance Altered", Robot.wrist.wristEncoder.getDistance());
     drivetrain.putEncoderValues();
   }
